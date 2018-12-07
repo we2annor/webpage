561b2a7c81f6004ab9a21e6f1addc79dae3440c8
ernest annor <we2annor@gmail.com>
2018-12-07 14:49:21 +0000

    Updates

---
+++ precache-manifest.5959af9cc4b7a70c0cf878f64552cda4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35c6d35963d4e3bd941d",
+    "url": "/webpage/static/js/runtime~main.35c6d359.js"
+  },
+  {
+    "revision": "a5814135ef727844aeab",
+    "url": "/webpage/static/js/main.a5814135.chunk.js"
+  },
+  {
+    "revision": "9068e3dfea403518d05d",
+    "url": "/webpage/static/js/1.9068e3df.chunk.js"
+  },
+  {
+    "revision": "a5814135ef727844aeab",
+    "url": "/webpage/static/css/main.fbb0c989.chunk.css"
+  },
+  {
+    "revision": "faa1a3110827ac64eaa2a36e92f424ef",
+    "url": "/webpage/index.html"
+  }
+];
